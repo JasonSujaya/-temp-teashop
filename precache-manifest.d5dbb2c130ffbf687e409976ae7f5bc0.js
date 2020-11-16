35107941e58aece2e7949fdb0d10db77bc6cf3f2
jasonsujaya <sujaya.jason@gmail.com>
2020-11-16 09:56:36 +0100

    Updates

---
+++ precache-manifest.d5dbb2c130ffbf687e409976ae7f5bc0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7151d06cabaac95bb5d08654dc12126",
+    "url": "/-temp-teashop/index.html"
+  },
+  {
+    "revision": "17e571aa3ec033c6348e",
+    "url": "/-temp-teashop/static/css/main.79bf5a43.chunk.css"
+  },
+  {
+    "revision": "2ca41b8d6fc2a5bd0ef0",
+    "url": "/-temp-teashop/static/js/2.2bf559ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/-temp-teashop/static/js/2.2bf559ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e571aa3ec033c6348e",
+    "url": "/-temp-teashop/static/js/main.ff14e3a0.chunk.js"
+  },
+  {
+    "revision": "dfb2809d736f087482a7",
+    "url": "/-temp-teashop/static/js/runtime-main.145413c7.js"
+  },
+  {
+    "revision": "3371769dcb5e107a7217ce04d2b8d801",
+    "url": "/-temp-teashop/static/media/default-monochrome-white.3371769d.svg"
+  },
+  {
+    "revision": "fc3f4b8f03777e6eae56d4bcde4dcfcc",
+    "url": "/-temp-teashop/static/media/open-menu.fc3f4b8f.svg"
+  },
+  {
+    "revision": "313772100dd5bc5a7e24559b756ec693",
+    "url": "/-temp-teashop/static/media/pot1.31377210.png"
+  },
+  {
+    "revision": "41a092f193a1fe6f518c111d647e9cdd",
+    "url": "/-temp-teashop/static/media/search.41a092f1.svg"
+  },
+  {
+    "revision": "ab639bfcca05ce3b385d75a8b7bbcbda",
+    "url": "/-temp-teashop/static/media/shopping-cart.ab639bfc.svg"
+  },
+  {
+    "revision": "24535dfcd64da917c27dcf339085e26b",
+    "url": "/-temp-teashop/static/media/tea-1.24535dfc.png"
+  },
+  {
+    "revision": "83501eed7bd6cac003fbae6deb2d45f7",
+    "url": "/-temp-teashop/static/media/tea-2.83501eed.png"
+  },
+  {
+    "revision": "dda70941731fe2e45c49b7a043eea20f",
+    "url": "/-temp-teashop/static/media/tea-3.dda70941.png"
+  }
+]);
